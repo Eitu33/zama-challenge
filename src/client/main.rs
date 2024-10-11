cda8f04fba52dd6c8231704b3f92c5ea979d641f
Thomas Plisson <thomasplisson@Thomass-MacBook-Pro.local>
2024-10-11 14:24:49 +0200

    Request handlers client side

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,21 +1,50 @@
+use std::fs;
+
+use clap::Parser;
 use http_body_util::BodyExt;
 use http_body_util::Full;
 use hyper::body::Bytes;
 use hyper::Request;
+use hyper::Uri;
 use hyper_util::rt::TokioIo;
 use mtree::MerkleTree;
-use std::fs;
-use tokio::io::{self, AsyncWriteExt as _};
+use tokio::io::{self, AsyncWriteExt};
 use tokio::net::TcpStream;
+
+#[derive(Parser)]
+enum Command {
+    Post { directory: String },
+    Get { file_index: u64 },
+}
+
+fn post_request(
+    url: Uri,
+    directory: String,
+) -> Result<Request<Full<Bytes>>, Box<dyn std::error::Error>> {
+    let mut files = Vec::new();
+    let dir = fs::read_dir(directory).unwrap();
+    for path in dir {
+        files.push(fs::read(path?.path())?);
+    }
+    fs::write("root", MerkleTree::new(&files).root().unwrap()).unwrap();
+    let bytes = Bytes::from(bincode::serialize(&files).unwrap());
+    Ok(Request::post(url).body(Full::new(bytes))?)
+}
+
+fn get_request(
+    url: Uri,
+    file_index: u64,
+) -> Result<Request<Full<Bytes>>, Box<dyn std::error::Error>> {
+    let bytes = Bytes::from(bincode::serialize(&file_index).unwrap());
+    Ok(Request::post(url).body(Full::new(bytes))?)
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let command = Command::parse();
     let url = "127.0.0.1:3000".parse::<hyper::Uri>()?;
-    let host = url.host().unwrap();
-    let port = url.port_u16().unwrap_or(80);
-    let stream = TcpStream::connect(format!("{}:{}", host, port)).await?;
+    let stream = TcpStream::connect(url.to_string()).await?;
     let io = TokioIo::new(stream);
-
     let (mut sender, conn) = hyper::client::conn::http1::handshake(io).await?;
 
     tokio::task::spawn(async move {
@@ -24,17 +53,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     });
 
-    let mut files = Vec::new();
-    let dir = fs::read_dir("data/client").unwrap();
-    for path in dir {
-        files.push(fs::read(path?.path())?);
-    }
-    fs::write("data/root", MerkleTree::new(&files).root().unwrap()).unwrap();
-    let bytes = bincode::serialize(&files).unwrap();
-
-    let req = Request::post(url)
-        .body(Full::new(Bytes::from(bytes)))
-        .unwrap();
+    let req = match command {
+        Command::Post { directory } => post_request(url, directory),
+        Command::Get { file_index } => get_request(url, file_index),
+    }?;
 
     let mut res = sender.send_request(req).await?;
     println!("Response status: {}", res.status());
